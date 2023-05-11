8309a01765ca3db8efef2565121e5dd06738093f
Manuel Grundner <m.grundner@delegate.at>
2023-05-11 08:56:16 +0400

    feat: debugable solution created

--- demo/BlazorComponentToCSharp.Demo/Program.cs
+++ demo/BlazorComponentToCSharp.Demo/Program.cs
@@ -4,10 +4,30 @@ using BlazorComponentDemoLibrary;
 using Spectre.Console;
 
 using XpoRecords.Demo.Helpers;
+using Microsoft.Build.Locator;
+using Microsoft.CodeAnalysis.MSBuild;
+
+MSBuildLocator.RegisterDefaults();
 
 AnsiConsole.WriteLine("Hello, World!");
 
-var directory = Path.GetDirectoryName(typeof(Program).Assembly.Location);
+var directory = Path.GetDirectoryName(typeof(Program).Assembly.Location)!;
+var solutionPath = $"../{nameof(BlazorComponentDemoLibrary)}/{nameof(BlazorComponentDemoLibrary)}.csproj";
+
+using var workspace = MSBuildWorkspace.Create();
+
+// Print message for WorkspaceFailed event to help diagnosing project load failures.
+workspace.WorkspaceFailed += (o, e) => AnsiConsole.MarkupLineInterpolated($"[red]{e.Diagnostic.Message}[/]");
+
+AnsiConsole.MarkupLineInterpolated($"Loading solution '{solutionPath}'");
+
+// Attach progress reporter so we print projects as they are loaded.
+var solution = await workspace.OpenProjectAsync(solutionPath, new ConsoleProgressReporter());
+Console.WriteLine($"Finished loading solution '{solutionPath}'");
+
+// TODO: Do analysis on the projects in the loaded solution
+
+
 
 var razorFilePath = Path.Combine(directory, $"{nameof(Component1)}.razor");
 var razorFileContent = File.ReadAllText(razorFilePath);
@@ -26,4 +46,18 @@ static void WriteFileContent(string caption, string content, string lang = "cs")
     AnsiConsole.WriteLine(new string('-', Console.WindowWidth));
     ConsoleHelper.PrintSource(content, lang);
     AnsiConsole.WriteLine(new string('-', Console.WindowWidth));
+}
+
+internal class ConsoleProgressReporter : IProgress<ProjectLoadProgress>
+{
+    public void Report(ProjectLoadProgress loadProgress)
+    {
+        var projectDisplay = Path.GetFileName(loadProgress.FilePath);
+        if (loadProgress.TargetFramework != null)
+        {
+            projectDisplay += $" ({loadProgress.TargetFramework})";
+        }
+
+        AnsiConsole.MarkupLineInterpolated($"[white]{loadProgress.Operation,-15}[/] [lightgreen]{loadProgress.ElapsedTime,-15:m\\:ss\\.fffffff}[/] [gray]{projectDisplay}[/]");
+    }
 }
--- src/BlazorComponentToCSharp/BlazorComponentToCSharpEngine.cs
+++ src/BlazorComponentToCSharp/BlazorComponentToCSharpEngine.cs
@@ -11,7 +11,6 @@ public class BlazorComponentToCSharpEngine
 {
     public static string BlazorComponentToCSharp(string filePath, string fileContent)
     {
-
         return "";
     }
 
